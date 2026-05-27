b75be4093fd1bca3c7142d6034db74b83431cc36
asingizwe1 <asingizwe1louis1@gmail.com>
2026-05-27 03:33:05 +0300

    adding hex crate to cargo.toml to help in converting string to bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+#[allow(unused)]
+fn read_version(transaction_hex:&str)->u32{return 1;}
+
+
 fn main() {
-    println!("Hello, world!");
-}
+   let version = read_version("0200000001b1ed1c8e5b9a1c3f0e5b6a7c8d9e0f1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n1o2p3q4r5s6t7u8v9w0x1y2z3a4b5c6d7e8f9g0h1i2j3k4l5m6n7o8p9q0r1s2t3u4v5w6x7y8z9a0b1c2d3e4f5g6h7i8j9k0l1m2n3o4p5q6r7s8t9u0v1w2x3y4z5a6b7c8d9e0f1g2h3i4j5k6l7m8n9o0p1q2r3s4t5u6v7w8x9y0z1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7a8b9c0d1e2f3g4h5i6j7k8l9m0n1o2p3q4r5s6t7u8v9w0x1y2z3a4b5c6d7e8f9g0h1i2j3k4l5m6n7o8p9q0r1s2t3u4v5w6x7y8z9a0b1c2d3e4f5g6h7i8j9k0l1m2n3o4p5q6r7s8t9u0v1w2x3y4z5a6b7);
+println!("Version: {}", version);
+   }
+//we use a vec instead of array coz of stack allocation issues, we can use array if we know the size beforehand, but in this case we don't know the size of the transaction hex string, so we use a vec to store the bytes of the transaction.
+//converting a hex string into vec
+//we use the hex crate to convert the hex string into bytes, we can use the from_hex function to convert the hex string into a vec of bytes, and then we can use the vec of bytes to read the version of the transaction.
