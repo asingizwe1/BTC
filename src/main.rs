e027cc56c67013946099086fa81e8c5802399838
asingizwe1 <asingizwe1louis1@gmail.com>
2026-05-27 03:38:50 +0300

    setting up read_version function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 
 #[allow(unused)]
-fn read_version(transaction_hex:&str)->u32{return 1;}
+fn read_version(transaction_hex:&str)->u32{
+    let transaction_bytes=hex::decode(transaction_hex).expect("Invalid hex string");
+    let version_bytes=transaction_bytes[0..4];//for first 4 bytes
+   println!("version bytes: {:?}", version_bytes);
+   // return 1;
+    //you can as well write as 1 since the last statement after colon will be returned
+1   //idiomatic return
+}
 
 
 fn main() {
