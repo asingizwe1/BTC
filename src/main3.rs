06f6ae6eb0816da2b22539c9d78742064d71fa5f
asingizwe1 <asingizwe1louis1@gmail.com>
2026-06-06 01:48:48 +0300

    btc detailing

--- src/main3.rs
+++ src/main3.rs
@@ -14,3 +14,7 @@ fn calculate_len(mut arr: [u8; 4]) -> usize {
 //at any point you can have one writer or mulitple readers
 //you can have a SINGLE mutable reference, you cant read/ modify a vector at the same time
 //you cant have a reference and a mutable reference of the same object at the same time
+//   your wallet does not have a single "account balance". Instead,
+//   your balance is a collection of discrete chunks of Bitcoin that were sent to you. */
+//It serves as an exact "map" to locate the source funds you are attempting to spend
+//in btc every transaction input points to a previous output - pointer has 2 pieces of info which are txid(String) and vout(output index)(u32)-[because you can have multiple transactions] - output index in that transaction *Because Bitcoin uses the UTXO (Unspent Transaction Output) model,
