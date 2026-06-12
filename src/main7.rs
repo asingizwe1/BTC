eaedcad4f533fb4eccb27e67bff48291a034155b
asingizwe1 <asingizwe1louis1@gmail.com>
2026-06-12 14:02:09 +0300

    usage of usize as per rust

--- src/main7.rs
+++ src/main7.rs
@@ -14,7 +14,7 @@ fn read_32(transaction_bytes: &mut &[u8]) -> u32 {
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     let mut compact_size = [0u8; 1]; // Read the first marker byte
     transaction_bytes.read(&mut compact_size).unwrap();
-
+    //read_compact_size returns a u64 because Bitcoin’s CompactSize can represent very large numbers (up to 2^64).
     match compact_size[0] {
         0..=252 => compact_size[0] as u64, // Direct value
         253 => {
@@ -30,7 +30,7 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
         255 => {
             let mut buffer = [0u8; 8]; // Next 8 bytes → u64
             transaction_bytes.read(&mut buffer).unwrap();
-            u64::from_le_bytes(buffer)
+            u64::from_le_bytes(buffer) //interprete the bytes as little endian
         }
     }
 }
@@ -46,6 +46,20 @@ fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
     // Return the raw txid bytes
 }
 
+//mut &[u8] -> any dynamically sized contiguous data
+//since the script size isnt known
+fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
+    let script_size = read_compact_size(&mut bytes_slice) as usize;
+    let mut buffer = vec![0_u8; script_size]; //the length parameter must be a usize, not u64.
+    //usize is the type Rust uses for memory sizes and indexing.
+    /*On a 32‑bit system → usize is 32 bits.
+
+    This ensures that when you say “make a vector of length N,” the compiler knows how to handle it safely in memory. */
+    transaction_bytes.read(&mut buffer).unwrap();
+    //if type doesnt match rust coul try to dereference the object
+    buffer
+}
+
 fn main() {
     // Example transaction hex string (truncated for demo)
     let transaction_hex = "01000000024d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc29";
@@ -57,15 +71,16 @@ fn main() {
     let mut bytes_slice = transaction_bytes.as_slice();
 
     // Read the version (first 4 bytes)
-    let version = read_version(&mut bytes_slice);
+    let version = read_u32(&mut bytes_slice);
 
     // Read the input count (CompactSize format)
     let input_count = read_compact_size(&mut bytes_slice);
 
     // Loop through each input and read its txid
     for _ in 0..input_count {
+        //for the output index we call u32
+        let output_index = read_u32(&mut bytes_slice);
         let txid = read_txid(&mut bytes_slice);
-        println!("TxID: {:?}", txid); // Print raw txid bytes
     }
 
     // Print parsed values
